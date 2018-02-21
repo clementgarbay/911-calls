5b4022397e9d84dd5a393f0cd00b9aef00b257c7
Clément Garbay <clementgarbay@gmail.com>
2018-02-21 11:46:30 +0100

    Update fields names in mongo import script

--- mongodb/import.js
+++ mongodb/import.js
@@ -12,14 +12,14 @@ const insertCalls = function(callsCollection, callback) {
         .pipe(csv())
         .on('data', data => {
             calls.push({
-                loc : {
+                location: {
                     type: "Point",
                     coordinates: [parseFloat(data.lng.trim()), parseFloat(data.lat.trim())]
                 },
                 desc: data.desc.trim(),
                 zip: data.zip.trim(),
-                title_cat: data.title.split(':')[0].trim(),
-                title_descr: data.title.split(':')[1].trim(),
+                category: data.title.split(':')[0].trim(),
+                title: data.title.split(':')[1].trim(),
                 timeStamp: data.timeStamp.trim(),
                 twp: data.twp.trim(),
                 addr: data.addr.trim(),
@@ -36,8 +36,8 @@ const insertCalls = function(callsCollection, callback) {
 MongoClient.connect(mongoUrl, (err, db) => {
     const callsCollection = db.collection('calls');
 
-    callsCollection.createIndex({ loc : "2dsphere" });
-    callsCollection.createIndex({ title_descr : "text" });
+    callsCollection.createIndex({ location : "2dsphere" });
+    callsCollection.createIndex({ title : "text" });
 
     insertCalls(callsCollection, result => {
         console.log(`${result.insertedCount} calls inserted`);
