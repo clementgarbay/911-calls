765d7bfe5f862ff3323e62087c9e446454760734
Prygan <prygan@localhost.localdomain>
2018-02-21 10:10:14 +0100

    completed ES data import script to import data from the provided CSV file

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,52 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+esClient.indices.create({ index: 'calls' }, (err, resp) => {
+  if (err) console.trace(err.message);
+});
+
+let calls = [];
+
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      // TODO extract one line from CSV
+      calls.push({
+        lat: data.lat,
+        lng: data.lng,
+        desc: data.desc,
+        zip: data.zip,
+        title: data.title,
+        timeStamp: data.timeStamp,
+        twp: data.twp,
+        addr: data.addr,
+        e: data.e
+      });
     })
     .on('end', () => {
-      // TODO insert data to ES
-    });
+      esClient.bulk(createBulkInsertQuery(calls), (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`Inserted ${resp.items.length} 911 calls`);
+        esClient.close();
+      });
+});
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(calls) {
+  const body = calls.reduce((acc, call) => {
+    const { lat,lng,desc,zip,title_cat,title_descr,timeStamp,twp,addr,e } = call;
+    acc.push({ index: { _index: 'calls', _type: 'call'} });
+    acc.push({ 
+      lat: lat,
+      lon: lng,
+      desc,
+      zip,
+      title,
+      timeStamp,
+      twp,
+      addr,
+      e });
+    return acc;
+  }, []);
+
+  return { body };
+}
